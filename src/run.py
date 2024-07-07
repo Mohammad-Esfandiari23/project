2f16be7daf5c44438b099507805c0ac70da92022
Mohammad Esfandiari <mohammadesfandiari32@gmail.com>
2024-07-07 16:30:36 +0330

    v.06

--- src/run.py
+++ src/run.py
@@ -2,4 +2,24 @@ import streamlit as st
 
 st.title("Dashbord")
 st.text("here im trying to do some shit")
-st.text("continueing to do this shit")
+
+import pandas as pd
+from io import StringIO
+
+uploaded_file = st.file_uploader("choose what you want, i don't care, whatever")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
